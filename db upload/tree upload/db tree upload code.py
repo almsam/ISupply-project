7fe8ace90cebdc8d51771839efea5c1e13a04470
almsam <44277999+almsam@users.noreply.github.com>
2024-07-30 16:10:34 -0700

    df process

--- db upload/tree upload/db tree upload code.py
+++ db upload/tree upload/db tree upload code.py
@@ -15,3 +15,21 @@ out = cursor.fetchall()
 print(out)
 cursor.close()
 con.close()
+
+df = pd.DataFrame(out, columns=["ser", "cat"])
+dfT = pd.read_excel("slimTree.xlsx")
+dfT.fillna("-1", inplace=True)
+dfT.head(15)
+
+map_dic = df.set_index("cat")["ser"].to_dict()
+
+dfInt = pd.DataFrame(dfT)
+
+dfInt["category"] = dfInt["category"].map(map_dic).fillna(-1).astype(int)
+dfInt["subcategory"] = dfInt["subcategory"].map(map_dic).fillna(-1).astype(int)
+
+print("The dataframe has ", dfInt.size, "/27564 entries")
+
+dfIntUnique = pd.DataFrame(dfInt.drop_duplicates())
+
+print("Now the dataframe has ", dfIntUnique.size, "/15400 entries")
