8aadbf512a5df66ccb977c742d2ca566b4b17102
almsam <44277999+almsam@users.noreply.github.com>
2024-12-16 15:52:44 -0800

    modify csv line wise

--- db upload/Map upload/CSV modifer.py
+++ db upload/Map upload/CSV modifer.py
@@ -15,11 +15,26 @@ def modify_csv(input_file, output_file):
                         if i not in {len(cell)}#, 12, 25, len(cell) - 1}
                     )
                 else:
-                    modified = cell  # Keep short strings unchanged
+                    modified = cell  # keep short strings unchanged
                 modified_row.append(modified)
             writer.writerow(modified_row)
+
+
+def modify_csv_lines(input_file, output_file):
+    with open(input_file, 'r') as infile, open(output_file, 'w') as outfile:
+        for line in infile:
+            line = line.strip()  # remove head whitespace
+            if len(line) > 1: #safety case
+                    # remove 1st, 14th, 26th, and -1st characters
+                modified_line = ''.join(
+                    char for i, char in enumerate(line)
+                    if i not in {0, 13, 25, len(line) - 1}
+                )
+                outfile.write(modified_line + '\n')
+            else:
+                outfile.write(line + '\n')  # return unchanged if line is too short
 
 # run as a method for clean code reasons
 input_csv = "C:/Users/samia/OneDrive/Desktop/ISupply-project/db upload/Map upload/query_list.csv"
 output_csv = "C:/Users/samia/OneDrive/Desktop/ISupply-project/db upload/Map upload/query_list_clean.csv"
-modify_csv(input_csv, output_csv)
+modify_csv_lines(input_csv, output_csv)
