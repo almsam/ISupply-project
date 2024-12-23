18f02fa97f1917d2ed4241dc56c4198fc2c83ad4
almsam <44277999+almsam@users.noreply.github.com>
2024-12-23 13:28:12 -0800

    removed dead code

--- db upload/Map upload/generate map sql.py
+++ db upload/Map upload/generate map sql.py
@@ -17,21 +17,7 @@ def process_categories(df, start, end):
 
     print(len(queryList)); return queryList
 
-def modify_csv(input_file, output_file):
-    with open(input_file, 'r') as infile, open(output_file, 'w', newline='') as outfile:
-        reader = csv.reader(infile); writer = csv.writer(outfile)
-        for row in reader:
-            modified_row = []
-            for cell in row:
-                if len(cell) > 1: #safety case
-                    # remove 1st, 14th, 26th, and -1st characters
-                    modified = ''.join(
-                        char for i, char in enumerate(cell); if i not in {len(cell)}#, 12, 25, len(cell) - 1}
-                    )
-                else:
-                    modified = cell  # keep short strings unchanged
-                modified_row.append(modified)
-            writer.writerow(modified_row)
+
 
 def clean_line(line):
     line = line.replace("\\", "") # remove all backslashes
