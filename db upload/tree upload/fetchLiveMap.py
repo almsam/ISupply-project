7c04e9f2e464f7b0df081bf117e4b2cc8985f751
almsam <44277999+almsam@users.noreply.github.com>
2024-12-31 15:44:26 -0800

    sql read code

--- db upload/tree upload/fetchLiveMap.py
+++ db upload/tree upload/fetchLiveMap.py
@@ -2,3 +2,19 @@ import pandas as pd
 import psycopg2
 import numpy as np
 
+con = psycopg2.connect(
+    database="iSupply",
+    host="38.180.117.52",
+    user="postgres",
+    password="deerRun",
+    port="5432",
+)
+cursor = con.cursor()
+cursor.execute("""select * from "Categories" c""")
+out = cursor.fetchall()  # make a dictionary of what the db currently has
+cursor.close()
+con.close()
+
+map = pd.DataFrame(out, columns=["ser", "cat"])
+
+print(map)
