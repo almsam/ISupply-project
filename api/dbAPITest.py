4ee86b3bb49af52f772dde34c8bf734f35e1816a
almsam <44277999+almsam@users.noreply.github.com>
2025-01-03 17:44:38 -0800

    testing done

--- api/dbAPITest.py
+++ api/dbAPITest.py
@@ -7,6 +7,20 @@ from dbAPI import setup
 
 class TestSetupFunction(unittest.TestCase):
 
+    def test_setup_outputs(self):
+        map, tree = setup() # call setup func
+        
+        # test output is df:
+        self.assertIsInstance(map,  pd.DataFrame, "map is not a pd df")
+        self.assertIsInstance(tree, pd.DataFrame, "tree is not a pd df")
+        
+        # test non empty:
+        self.assertGreater(len(map),  0, "map df is empty")
+        self.assertGreater(len(tree), 0, "tree df is empty")
+        
+        # verify columns:
+        self.assertListEqual(list(map.columns),  ["ser", "cat"],          "map df columns incorrect")
+        self.assertListEqual(list(tree.columns), ["id", "cat", "subcat"], "tree df columns incorrect")
 
 if __name__ == "__main__":
     unittest.main()
