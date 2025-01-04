0153eb25e6bb4251581acc59ffe18a7ce8e47a30
almsam <44277999+almsam@users.noreply.github.com>
2025-01-03 17:44:31 -0800

    import & class

---
+++ api/dbAPITest.py
@@ -0,0 +1,12 @@
+import unittest
+import pandas as pd
+from unittest.mock import patch
+import psycopg2
+
+from dbAPI import setup
+
+class TestSetupFunction(unittest.TestCase):
+
+
+if __name__ == "__main__":
+    unittest.main()
